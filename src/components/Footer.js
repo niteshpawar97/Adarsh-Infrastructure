00523bda4a93f2253dd3bfba70fb59342eefbfe6
niteshpawar97 <niteshpawar97@gmail.com>
2025-02-12 10:20:00 +0530

    🚀 Initial Launch: Adarsh Infrastructure Website Live!

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,14 @@
+import Link from "next/link";
+
+
 const Footer = () => {
   return (
     <footer className="bg-footer-bg text-footer-text py-6 px-8 flex flex-col md:flex-row justify-between items-center text-center md:text-left w-full shadow-lg">
       <p className="text-lg font-semibold">&copy; {new Date().getFullYear()} Adarsh Infrastructure. All Rights Reserved.</p>
       <div className="mt-4 md:mt-0 flex space-x-6">
-        <a href="/privacy-policy" className="hover:text-footer-hover transition">Privacy Policy</a>
-        <a href="/terms-of-service" className="hover:text-footer-hover transition">Terms of Service</a>
-        <a href="/contact" className="hover:text-footer-hover transition">Contact Us</a>
+        <Link href="/privacy-policy" className="hover:text-footer-hover transition">Privacy Policy</Link>
+        <Link href="/terms-of-service" className="hover:text-footer-hover transition">Terms of Service</Link>
+        <Link href="/contact" className="hover:text-footer-hover transition">Contact Us</Link>
       </div>
     </footer>
 
